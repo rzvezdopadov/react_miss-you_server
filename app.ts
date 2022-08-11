ea96a4fe23d69bb9544b98b30dd4c7674aa0551d
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-08-12 00:52:27 +0300

    first realisation added dot start to app

---
+++ app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+const config = require('config');
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json())
+
+app.use('/', require('./api/api'))
+
+const PORT = config.get('port') || 5000
+
+async function startServer() {
+    try {
+        app.listen(PORT, () => {
+            console.log(`App started on port: "${PORT}"`)
+        })
+    } catch (e) {
+        console.log("Server error with:",e.message)
+    }
+}
+
+startServer()
