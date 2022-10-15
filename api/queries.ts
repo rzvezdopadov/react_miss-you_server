b01da8b8c47d2807d667301b0b8736528900adcd
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-10-15 16:09:19 +0300

    change function set profiles

--- api/queries.ts
+++ api/queries.ts
@@ -34,7 +34,7 @@ export async function getPasswordByIdFromDB(id: number) {
     return '';
 }
 
-const fieldProfile = 'id, name, latitude, longitude, location, ' +
+const fieldProfile = 'id, timecode, name, latitude, longitude, location, ' +
 'likes, age, birthday, monthofbirth, yearofbirth, growth, weight, ' +
 'gender, gendervapor, photomain, photolink, signzodiac, ' +
 'education, fieldofactivity, maritalstatus, children, religion, ' +
@@ -68,7 +68,7 @@ export async function getProfileByIdFromDB(id: number) {
     return {};
 }
 
-const fieldProfileShort = 'id, name, age, photomain, photolink, interests';
+const fieldProfileShort = 'id, timecode, name, age, gender, photomain, photolink, interests';
 
 export async function getProfiles(QueryGetProfiles: IGetProfiles) {
     let countProfiles = 0;
@@ -115,6 +115,14 @@ export async function getProfiles(QueryGetProfiles: IGetProfiles) {
             }
 
             queryStr += '(id <> $13)';
+
+            let gendervapor = filters.gendervapor;
+
+            if (filters.gendervapor === 0) {
+                gendervapor = 1;
+            } else if (filters.gendervapor === 1) {
+                gendervapor = 0;
+            }
             
             answerDB = await pool.query(queryStr, [
                 filters.location, 
@@ -122,7 +130,7 @@ export async function getProfiles(QueryGetProfiles: IGetProfiles) {
                 filters.agestart, filters.ageend,
                 filters.growthstart, filters.growthend,
                 filters.weightstart, filters.weightend,
-                filters.gendervapor,
+                gendervapor,
                 filters.religion,
                 filters.smoke,
                 filters.alcohol,
@@ -149,18 +157,77 @@ export async function getProfiles(QueryGetProfiles: IGetProfiles) {
     return [];
 }
 
-export function setProfile(profile: IProfile) {
-    // for (let i = 0; i < userList.length; i++) {
-    //     if (userList[i].id === profile.id) {
-    //         for (let key in profile) {
-    //             userList[i][key] = profile[key];
-    //         }
+export async function setProfileByIdToDB(id: number, profile: IProfile) {
+    try {
+        let queryStrProfile = 'UPDATE users SET ';
+        
+        queryStrProfile += 'name = $2, location = $3, age = $4, ';
+        queryStrProfile += 'birthday = $5, monthofbirth = $6, yearofbirth = $7, ';
+        queryStrProfile += 'growth = $8, weight = $9, ';
+        queryStrProfile += 'gender = $10, gendervapor = $11, ';
+        queryStrProfile += 'signzodiac = $12, education = $13, ';
+        queryStrProfile += 'fieldofactivity = $14, maritalstatus = $15, ';
+        queryStrProfile += 'children = $16, religion = $17, ';
+        queryStrProfile += 'smoke = $18, alcohol = $19, ';
+        queryStrProfile += 'discription = $20, profit = $21, ';
+        queryStrProfile += 'interests = $22, ';
+        queryStrProfile += 'ilikecharacter = $23, idontlikecharacter = $24 ';
+        queryStrProfile += 'WHERE id = $1';
 
-    //         return true;
-    //     }
-    // }
+        const answerDBProfile = await pool.query(queryStrProfile, [id,
+            profile.name, profile.location, profile.age,
+            profile.birthday, profile.monthofbirth, profile.yearofbirth,  
+            profile.growth, profile.weight,
+            profile.gender, profile.gendervapor,
+            profile.signzodiac, profile.education,
+            profile.fieldofactivity, profile.maritalstatus,
+            profile.children, profile.religion,
+            profile.smoke, profile.alcohol,
+            profile.discription, profile.profit,
+            profile.interests,
+            profile.ilikecharacter, profile.idontlikecharacter,         
+        ]);
 
-    return false;
+        let queryStrFilters = 'UPDATE filters SET ';
+
+        queryStrFilters += 'location = $2, ';
+        queryStrFilters += 'agestart = $3, ageend = $4, ';
+        queryStrFilters += 'growthstart = $5, growthend = $6, ';
+        queryStrFilters += 'weightstart = $7, weightend = $8, ';
+        queryStrFilters += 'signzodiac = $9, ';
+        queryStrFilters += 'gendervapor = $10, ';
+        queryStrFilters += 'religion = $11, ';
+        queryStrFilters += 'smoke = $12, ';
+        queryStrFilters += 'alcohol = $13, ';
+        queryStrFilters += 'interests = $14 ';
+        queryStrFilters += 'WHERE id = $1';
+
+        const answerDBFilters = await pool.query(queryStrFilters, [id, 
+            profile.filters.location, 
+            profile.filters.agestart, profile.filters.ageend,
+            profile.filters.growthstart, profile.filters.growthend,
+            profile.filters.weightstart, profile.filters.weightend,
+            profile.filters.signzodiac, 
+            profile.filters.gendervapor,
+            profile.filters.religion,
+            profile.filters.smoke,
+            profile.filters.alcohol,
+            profile.filters.interests,
+        ]);
+    } catch (error) {
+        console.log('setProfileByIdToDB:', error);
+    }
+
+    let newProfile = {};
+
+    try {
+        newProfile = await getProfileByIdFromDB(id);
+    } catch (error) {
+        console.log('setProfileByIdToDB get:', error);
+        
+    }
+
+    return newProfile;
 }
 
 export function setProfileShort(profile: IProfile) {
@@ -181,10 +248,25 @@ export async function setJWTToDB(id: number, jwt: string) { // Set JWT in DB
 
         return answerDB.rowCount;
     } catch (error) {
-        console.log(error);
+        console.log('setProfileByIdToDB:', error);
     }
    
     return 0;
+}
+
+export async function setTimecodeToDB(id: number) { // Set Time code in DB
+    const date = new Date();
+    const timecode = date.getTime();
+
+    try {
+        const answerDB = await pool.query("UPDATE users SET timecode = $2 WHERE id = $1", [id, timecode]);
+
+        return answerDB.rowCount;
+    } catch (error) {
+        console.log('setTimecodeToDB:', error);
+    }
+   
+    return timecode;
 }
 
 export async function getJWTFromDB(id: number) { // Get JWT in DB
@@ -193,7 +275,7 @@ export async function getJWTFromDB(id: number) { // Get JWT in DB
 
         return answerDB.rows[0].jwt;
     } catch (error) {
-        console.log(error);
+        console.log('getJWTFromDB:', error);
     }
    
     return '';
