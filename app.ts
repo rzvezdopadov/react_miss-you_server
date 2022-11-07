3b4c7e37b970dd0c1af746345304711ee2d49f7b
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-11-07 21:34:08 +0300

    added socket

--- app.ts
+++ app.ts
@@ -2,7 +2,6 @@ import express from "express";
 const config = require('config');
 const cookieParser = require('cookie-parser');
 
-
 const app = express();
 
 app.use(cookieParser());
@@ -12,11 +11,43 @@ app.use(express.json())
 app.use('/', require('./api/api'));
 
 const PORT = config.get('port') || 5000;
+const socketPORT = config.get('socketPort') || 8000; 
+
+const http = require('http').Server(app);
+const socketIO = require('socket.io')(http);
+
+socketIO.on('connection', function(socket) {
+    console.log('user connected');
+    
+    socket.on('disconnect', function () {
+        console.log('A user disconnected');
+    });
+})
+
+socketIO.on('message', function(socket) {
+    console.log('user message');
+    
+    socket.on('disconnect', function () {
+        console.log('A user disconnected');
+    });
+})
+
+socketIO.on('like', function(socket) {
+    console.log('user like');
+    
+    socket.on('disconnect', function () {
+        console.log('A user disconnected');
+    });
+})
+
+http.listen(socketPORT, function() {
+    console.log(`Socket started on port: "${socketPORT}"`);
+ });
 
 async function startServer() {
     try {
         app.listen(PORT, () => {
-            console.log(`App started on port: "${PORT}"`);
+            console.log(`Web started on port: "${PORT}"`);
         })
     } catch (e) {
         console.log("Server error with:",e.message);
