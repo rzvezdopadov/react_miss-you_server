eddfeeede9d71fa98e513636e4aaad1b4e0f3cd9
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-12-02 19:15:39 +0300

    optimization fn

--- api/profile.ts
+++ api/profile.ts
@@ -5,105 +5,45 @@ import {
 	IQueryGetProfiles,
 } from "../interfaces/iprofiles";
 import { getSignZodiac } from "../utils/profile";
-import { testOverflowJWT } from "../utils/token";
-import { getJWTFromDB, setTimecodeToDB } from "../query/auth";
+import { testToken } from "../utils/token";
 import {
 	getProfileByIdFromDB,
 	getProfiles,
 	setProfileByIdToDB,
 } from "../query/profile";
 
-const config = require("config");
-const jwtToken = require("jsonwebtoken");
-
 export async function querySetProfile(req, res) {
 	try {
 		const { profile } = req.body;
-
-		const { jwt } = req.cookies;
-
 		profile as IProfile;
 
-		const decode = await jwtToken.verify(jwt, config.get("jwtSecret"));
+		let { jwt } = req.cookies;
+		jwt = String(jwt);
 
-		if (jwt && profile && testOverflowJWT(decode.exp)) {
+		const jwtDecode = await testToken(jwt);
+
+		if (!jwtDecode)
 			return res.status(400).json({
-				message: "Токен просрочен, повторите вход в систему!",
+				message: "Токен не валидный!",
 			});
-		}
 
-		const token = getJWTFromDB(decode.userId);
+		profile.signzodiac = getSignZodiac(
+			profile.birthday,
+			profile.monthofbirth
+		);
 
-		token
-			.then((token) => {
-				if (token !== jwt) {
-					return res
-						.status(400)
-						.json({ message: "Токен не валидный!" });
-				}
+		const newProfile = await setProfileByIdToDB(jwtDecode.userId, profile);
 
-				setTimecodeToDB(decode.userId);
-
-				profile.signzodiac = getSignZodiac(
-					profile.birthday,
-					profile.monthofbirth
-				);
-
-				const newProfile = setProfileByIdToDB(decode.userId, profile);
-
-				newProfile
-					.then((profile) => {
-						return res.status(200).json(profile);
-					})
-					.catch((error) => {
-						console.log(error);
-
-						return res
-							.status(400)
-							.json({ message: "Произошла ошибка!" });
-					});
-			})
-			.catch((error) => {
-				console.log(error);
-
-				return res
-					.status(400)
-					.json({ message: "При авторизации произошла ошибка TBD!" });
-			});
+		return res.status(200).json(newProfile);
 	} catch (e) {
 		res.status(500).json({
-			message: "Токен не валидный!",
+			message: "Ошибка QTDB!",
 		});
 	}
 }
 
 export async function querySetProfileShort(req, res) {
 	try {
-		const { email, password } = req.body;
-
-		const { jwt } = req.cookies;
-
-		// const decode = await jwtToken.verify(jwt, config.get('jwtSecret'));
-
-		// if (getJWT(jwt) !== decode.userId) {
-		//     return res.status(400).json({ message:"Токен не валидный!" });
-		// }
-
-		// let profile = getProfileFromDB(decode.userId);
-
-		// profile.email = email;
-
-		// const hashedPassword = await bcrypt.hash(password, 13);
-		// profile.password = hashedPassword;
-
-		// if (!setProfileShort(profile)) {
-		//     return res.status(500).json({ message:"Профиль не изменен, возникли проблемы!" });
-		// }
-
-		// profile.jwt = '';
-		// profile.email = '';
-		// profile.password = '';
-
 		return res.status(200).json([]);
 	} catch (e) {
 		res.status(500).json({
@@ -115,70 +55,40 @@ export async function querySetProfileShort(req, res) {
 export async function queryGetProfile(req, res) {
 	try {
 		const QueryGetProfiles: IQueryGetProfile = req.query;
+		const id = Number(QueryGetProfiles.id);
 
-		const { jwt } = req.cookies;
-		const id = QueryGetProfiles.id;
+		let { jwt } = req.cookies;
+		jwt = String(jwt);
 
-		const decode = await jwtToken.verify(jwt, config.get("jwtSecret"));
+		const jwtDecode = await testToken(jwt);
 
-		if (testOverflowJWT(decode.exp)) {
+		if (!jwtDecode)
 			return res.status(400).json({
-				message: "Токен просрочен, повторите вход в систему!",
+				message: "Токен не валидный!",
 			});
+
+		let idNew = id;
+
+		if (idNew == 0) {
+			idNew = jwtDecode.userId;
 		}
 
-		const token = getJWTFromDB(decode.userId);
+		const profile = await getProfileByIdFromDB(idNew);
 
-		token
-			.then((token) => {
-				if (token !== jwt) {
-					return res
-						.status(400)
-						.json({ message: "Токен не валидный!" });
-				}
+		if (id != 0) {
+			const posId = profile.likes.indexOf(jwtDecode.userId);
 
-				setTimecodeToDB(decode.userId);
+			if (posId === -1) {
+				profile.likes = [];
+			} else {
+				profile.likes = [jwtDecode.userId];
+			}
+		}
 
-				let idNew = id;
-
-				if (idNew == 0) {
-					idNew = decode.userId;
-				}
-
-				const profile = getProfileByIdFromDB(idNew);
-
-				profile
-					.then((profile) => {
-						if (id != 0 && Object.keys(profile).length > 0) {
-							const posId = profile.likes.indexOf(decode.userId);
-
-							if (posId === -1) {
-								profile.likes = [];
-							} else {
-								profile.likes = [decode.userId];
-							}
-						}
-
-						return res.status(200).json(profile);
-					})
-					.catch((error) => {
-						console.log(error);
-
-						return res
-							.status(400)
-							.json({ message: "Произошла ошибка!" });
-					});
-			})
-			.catch((error) => {
-				console.log(error);
-
-				return res
-					.status(400)
-					.json({ message: "При авторизации произошла ошибка TBD!" });
-			});
+		return res.status(200).json(profile);
 	} catch (e) {
 		res.status(500).json({
-			message: "Токен не валидный!",
+			message: "Ошибка QTDB!",
 		});
 	}
 }
@@ -187,72 +97,42 @@ export async function queryGetProfiles(req, res) {
 	try {
 		const QueryGetProfiles: IQueryGetProfiles = req.query;
 
-		const { jwt } = req.cookies;
+		let { jwt } = req.cookies;
+		jwt = String(jwt);
 
-		const decode = await jwtToken.verify(jwt, config.get("jwtSecret"));
+		const jwtDecode = await testToken(jwt);
 
-		if (testOverflowJWT(decode.exp)) {
+		if (!jwtDecode)
 			return res.status(400).json({
-				message: "Токен просрочен, повторите вход в систему!",
+				message: "Токен не валидный!",
 			});
+
+		const getProfilesVal: IGetProfiles = {
+			jwt: jwt,
+			id: QueryGetProfiles.id,
+			startcount: QueryGetProfiles.startcount,
+			amount: QueryGetProfiles.amount,
+			filters: QueryGetProfiles.filters,
+			users: [],
+		};
+
+		const { filters, users } = QueryGetProfiles;
+
+		if (filters) {
+			const filtersParse = JSON.parse(filters as any);
+			getProfilesVal.filters = filtersParse;
 		}
 
-		const token = getJWTFromDB(decode.userId);
+		if (users) {
+			const usersParse: any = users.split(",");
+			getProfilesVal.users = usersParse;
+		}
 
-		token
-			.then((token) => {
-				if (token !== jwt) {
-					return res
-						.status(400)
-						.json({ message: "Токен не валидный!" });
-				}
+		getProfilesVal.id = jwtDecode.userId;
 
-				setTimecodeToDB(decode.userId);
+		const profiles = await getProfiles(getProfilesVal);
 
-				const getProfilesVal: IGetProfiles = {
-					jwt: jwt,
-					id: QueryGetProfiles.id,
-					startcount: QueryGetProfiles.startcount,
-					amount: QueryGetProfiles.amount,
-					filters: QueryGetProfiles.filters,
-					users: [],
-				};
-
-				const { filters, users } = QueryGetProfiles;
-
-				if (filters) {
-					const filtersParse = JSON.parse(filters as any);
-					getProfilesVal.filters = filtersParse;
-				}
-
-				if (users) {
-					const usersParse: any = users.split(",");
-					getProfilesVal.users = usersParse;
-				}
-
-				getProfilesVal.id = decode.userId;
-
-				const profiles = getProfiles(getProfilesVal);
-
-				profiles
-					.then((profiles) => {
-						return res.status(200).json(profiles);
-					})
-					.catch((error) => {
-						console.log(error);
-
-						return res
-							.status(400)
-							.json({ message: "Произошла ошибка!" });
-					});
-			})
-			.catch((error) => {
-				console.log(error);
-
-				return res
-					.status(400)
-					.json({ message: "При авторизации произошла ошибка TBD!" });
-			});
+		return res.status(200).json(profiles);
 	} catch (e) {
 		res.status(500).json({
 			message: "Токен не валидный!",
