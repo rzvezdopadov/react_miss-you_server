25ea9e1736fc2dbbf3aebe9cd1ecece25748a03f
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-01-06 19:39:43 +0300

    added new fn

--- utils/datetime.ts
+++ utils/datetime.ts
@@ -3,3 +3,9 @@ export function getTimecodeNow() {
 	const timecodeNow = dateNow.getTime();
 	return timecodeNow;
 }
+
+export const getYearFromAge = (year: number): number => {
+	const date = new Date();
+	const yearNow = date.getFullYear();
+	return yearNow - year;
+};
