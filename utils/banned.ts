112a5ea17d262e1f05d8635c14d799e59ebf10ce
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-02-19 18:08:35 +0300

    created test banned fn

---
+++ utils/banned.ts
@@ -0,0 +1,16 @@
+import { getAdminBannedByIdFromDB } from "../query/admin";
+import { getTimecodeNow } from "./datetime";
+
+export async function isBannedUser(ourId: string): Promise<string> {
+	const isBanned = await getAdminBannedByIdFromDB(ourId);
+
+	if (isBanned.timecode && isBanned.timecode > getTimecodeNow()) {
+		const date = new Date(Number(isBanned.timecode));
+
+		return `Ваш аккаунт был забанен ${isBanned.whobanned} по причине ${
+			isBanned.discription
+		}, ваш аккаунт будет разбанен ${date.toLocaleDateString()} в ${date.toLocaleTimeString()} по МСК`;
+	}
+
+	return "";
+}
