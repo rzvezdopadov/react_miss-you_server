51d2effa4aa12964dfc638b8f33a43a0dea25f1a
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-03-22 14:08:52 +0300

    added fn get time date

--- utils/datetime.ts
+++ utils/datetime.ts
@@ -9,3 +9,9 @@ export const getYearFromAge = (year: number): number => {
 	const yearNow = date.getFullYear();
 	return yearNow - year;
 };
+
+export function getTimedateNow(): string {
+	const dateNow = new Date();
+	const timecodeNow = `${dateNow.toLocaleDateString()} ${dateNow.toLocaleTimeString()}`;
+	return timecodeNow;
+}
