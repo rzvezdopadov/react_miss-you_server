fb266dc7d4ea2dbd7da30a9b7a3e61874a8a7342
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-03-24 22:03:33 +0300

    added universal fn for insert paid tariffs

--- microservices/shop/paid/paidDB.ts
+++ microservices/shop/paid/paidDB.ts
@@ -1,6 +1,6 @@
 import { poolDB } from "../../../db/config";
 import { getUniQueryFromDB } from "../../../db/uniquery";
-import { IPaid } from "./ipaid";
+import { IPaid, ITariff } from "./ipaid";
 
 export async function getPaidByIdFromDB(ourId: string): Promise<IPaid> {
 	try {
@@ -27,6 +27,40 @@ export async function setPaidByIdToDB(
 		return answerDB.rowCount;
 	} catch (error) {
 		console.log("setPaidToDB", error);
+		return 0;
+	}
+}
+
+export async function insertPaidTariffToDB(
+	paidname: string,
+	paidtariff: ITariff[]
+): Promise<number> {
+	try {
+		const paid = await getUniQueryFromDB(
+			"shop",
+			["name", "payload"],
+			[`name = '${paidname}'`]
+		);
+
+		if (!paid.length) {
+			let strTariffs = "";
+			paidtariff.forEach((value) => {
+				strTariffs += `'${JSON.stringify(value)}' :: JSON, `;
+			});
+
+			strTariffs = strTariffs.slice(0, -2);
+
+			let str = `INSERT INTO public.shop(name, payload) VALUES ('${paidname}', ARRAY [${strTariffs}]);`;
+
+			const answerDB = await poolDB.query(str);
+
+			return answerDB.rowCount;
+		}
+
+		return paid.length;
+	} catch (error) {
+		console.log("insertPaidByIdToDB", error);
+
 		return 0;
 	}
 }
