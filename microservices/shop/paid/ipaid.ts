b73ebdd0aceb6139e7be5be3936e85c58aa0d685
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-03-28 22:11:57 +0300

    added new property

--- microservices/shop/paid/ipaid.ts
+++ microservices/shop/paid/ipaid.ts
@@ -28,9 +28,35 @@ export interface IPaid {
 	historymessages300: IPaidContent;
 }
 
+export enum PAID_PROPERTY {
+	messageswrite = `messageswrite`,
+	messagesread = `messagesread`,
+	longfilters = `longfilters`,
+	filtersvapors = `filtersvapors`,
+	longfiltersvapors = `longfiltersvapors`,
+	filtersfavoriteusers = `filtersfavoriteusers`,
+	longfiltersfavoriteusers = `longfiltersfavoriteusers`,
+	photofull = `photofull`,
+	photoload10 = `photoload10`,
+	photoload15 = `photoload15`,
+	photoload20 = `photoload20`,
+	photoload25 = `photoload25`,
+	photoload30 = `photoload30`,
+	interests20 = `interests20`,
+	interests30 = `interests30`,
+	historymessages20 = `historymessages20`,
+	historymessages40 = `historymessages40`,
+	historymessages60 = `historymessages60`,
+	historymessages80 = `historymessages80`,
+	historymessages100 = `historymessages100`,
+	historymessages200 = `historymessages200`,
+	historymessages300 = `historymessages300`,
+}
+
 export interface ITariff {
 	idTariff: string;
-	amountDay: number;
+	amountDay?: number;
+	amountRate?: number;
 	price: number;
 	discount: number;
 }
