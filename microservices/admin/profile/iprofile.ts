b1450c66da702e649c0dd2f13a20970e1b070efa
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-05-22 14:04:32 +0300

    delete don`t use interface and add new

--- microservices/admin/profile/iprofile.ts
+++ microservices/admin/profile/iprofile.ts
@@ -1,4 +1,9 @@
-import { ICoordinates, IFilterUsers } from "../../user/profile/iprofile";
+import {
+	IAchivment,
+	ICoordinates,
+	IFilterUsers,
+	IPresent,
+} from "../../user/profile/iprofile";
 import { ACCTYPE } from "../../role/role";
 
 import { IStatVisit } from "../statistics/istatistics";
@@ -13,17 +18,6 @@ export interface IAdminBanned {
 	timecode: number;
 	whobanned: string;
 	discription: string;
-}
-
-export interface IAdminUserProfile {
-	email: string;
-	userid: string;
-	coordinates: ICoordinates[];
-	registrationdate: number;
-	likes: string[];
-	visit: IStatVisit[];
-	banned: IAdminBanned;
-	paid: IPaid;
 }
 
 export interface IAdminFilterUsers extends IFilterUsers {
@@ -49,4 +43,53 @@ export interface IQuerySetAdminBanned {
 export interface IQueryDeleteAdminPhoto {
 	userid: string;
 	photoPos: number;
+}
+
+export interface IAdminProfile {
+	userid: string;
+	email: string;
+	coordinates: ICoordinates;
+	registrationdate: number;
+	timecode: number;
+	name: string;
+	location: string;
+	phone: string;
+	likes: string[];
+	favoriteusers: string[];
+	privateselections: string[];
+	bannedusers: string[];
+	presents: IPresent[];
+	achivments: IAchivment[];
+	birthday: number;
+	monthofbirth: number;
+	yearofbirth: number;
+	growth: number;
+	weight: number;
+	gender: number;
+	gendervapor: number;
+	photomain: number;
+	photolink: string[];
+	signzodiac: number;
+	goaldate: number;
+	education: number;
+	fieldofactivity: number;
+	maritalstatus: number;
+	children: number;
+	religion: number;
+	smoke: number;
+	alcohol: number;
+	discription: string;
+	profit: number;
+	interests: string[];
+	filters: IFilterUsers;
+	ilikecharacter: number[];
+	idontlikecharacter: number[];
+	stickerpacks: string[];
+	rating: number;
+	cash: number;
+	acctype: ACCTYPE;
+	visit: IStatVisit;
+	paid: IPaid;
+	referral: string;
+	deleteacc: number;
 }
